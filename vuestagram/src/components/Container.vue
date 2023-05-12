f442f27c789e704f442622043db64d4eaf1ebf89
fkdslfjldskfjsd <dmy2060@naver.com>
2023-05-12 15:35:41 +0900

    study: 인스타그램 클론코딩

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Post/>
+    <!-- <Post/>
+    <Post/> -->
+  </div>
+</template>
+
+<script>
+import Post from './Posts.vue'
+export default {
+  name: 'containerWrap',
+  // props: {
+  //   datalist:Array
+  // },
+  components: {
+    Post
+  }
+}
+</script>
+
+<style>
+
+</style>
