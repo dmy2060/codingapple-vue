f472a946096fed70db3b150b73b365cf06d9750b
fkdslfjldskfjsd <dmy2060@naver.com>
2023-05-19 20:35:36 +0900

    study:서버없이 업로드한 이미지 다루기, 글 발행기능, 이미지 인스타그램 필터기능 만들기 강의 및 실습

--- vuestagram/src/App_4.vue
+++ vuestagram/src/App_4.vue
@@ -9,15 +9,22 @@
     <img src="./assets/logo.png" class="logo" />
   </div>
 
-  <Container :data="data" :step="step" :imgUrl="imgUrl"/>
+  <Container :data="data" :step="step"/>
   <button @click="more">더보기</button>
   <div class="footer">
     <ul class="footer-button-plus">
-      <input @change="upload" type="file" id="file" class="inputfile" />
+      <input type="file" id="file" class="inputfile" />
       <label for="file" class="input-plus">+</label>
     </ul>
  </div>
 
+ <!-- 탭 만들기 -->
+ <!-- <div v-if="step == 0">내용0</div>
+ <div v-if="step == 1">내용1</div>
+ <div v-if="step == 2">내용2</div>
+ <button @click="step = 0">버튼0</button>
+ <button @click="step = 1">버튼1</button>
+ <button @click="step = 2">버튼2</button> -->
 </template>
 
 <script>
@@ -33,7 +40,8 @@ export default {
       data: postdata,
       num:0,
       step:0,
-      imgUrl: ''
+      // 탭만들기 처음값
+      // step:0,
     }
   },
   components: {
@@ -47,14 +55,6 @@ export default {
         this.data.push(result.data);
         this.num++;
       })
-    },
-    upload(e){
-      let file = e.target.files;
-      console.log(file[0]);
-      let url = URL.createObjectURL(file[0]);
-      this.imgUrl = url;
-      this.step = 2;
-      console.log(url)
     },
   }
 }
@@ -139,7 +139,15 @@ ul {
 }
 
 </style>
-<!-- 6강 글 발행기능 만들기 -->
+<!-- 4강 탭 만들기 & 탭으로 사진 업로드 페이지 만들기-->
 <!--
-    1. 
+    1. 동적인 ui만드는 법
+      (1). 현재상태 데이터로 만들기
+      (2). 상태에 따라 html이 어떻게 보일지 만들기
+    2. 페이지 2개 만들어보기 (다른 페이지가 필요한 경우 vue-route도 가능)
+      다만, 현재는 탭기능으로도 가능하기 때문에 tab으로 만듬
+      단, 라우터는 뒤로가기 버튼이 스마트폰이나 웹에서 유용하게 쓸 수 있음!!!
+    3. 중요하거나 여러군데에서 많이 쓰일 것 같은 데이터는 최상단에 넣는게 좋음
+    4. v-if와 v-for는 함께 사용이 불가함
+    
 -->
--- vuestagram/vue.config.js
+++ vuestagram/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  rules: {
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "vue/no-use-v-if-with-v-for": "off"
+},
+lintOnSave : false,
 })
